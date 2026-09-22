cb1d17964edce8d7fb1bf1e49ba72099247957ff
Samir Perez <samu-samu@hotmail.es>
2026-09-22 15:14:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,7 @@ HTML = """
 <input id="msg" placeholder="Escribe precio, horario..."><button onclick="enviar()">Enviar</button>
 <br><br>
 <button onclick="enviarTexto('precio')">💰 Precio</button>
-<button onclick="enviarTexto('pago')" style="display:flex; align-items:center; gap:8px;">
-  <img src="nequi_transparent.png" style="width:28px; height:28px; object-fit:contain;">
-  <img src="bancolombia_transparent.png" style="width:28px; height:28px; object-fit:contain;">
-  <img src="efectivo_transparent.png" style="width:28px; height:28px; object-fit:contain;">
-  Medios de Pago</button>
+<button onclick="enviarTexto('pago')">💳 Medios de Pago</button>
 <button onclick="enviarTexto('horario')">🕒 Horario</button>
 <button onclick="enviarTexto('agendar')">📅 Agendar</button>
 </div>
